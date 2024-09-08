c85c5ae85c62fdf78b50d50329400b2e254b19cb
michael-lee-sk <e0543977@u.nus.edu>
2024-09-08 21:26:27 +0800

    Update BookingService.java

--- backend/src/main/java/com/example/coosroombookings/service/BookingService.java
+++ backend/src/main/java/com/example/coosroombookings/service/BookingService.java
@@ -1,5 +1,7 @@
 package com.example.coosroombookings.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import com.example.coosroombookings.model.Booking;
 import com.example.coosroombookings.repository.BookingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
