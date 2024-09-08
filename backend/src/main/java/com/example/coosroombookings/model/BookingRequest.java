649b76ba87cafc35edc9e7672bae551ee43122dd
michael-lee-sk <e0543977@u.nus.edu>
2024-09-08 15:54:22 +0800

    Change bookingrequest

--- backend/src/main/java/com/example/coosroombookings/model/BookingRequest.java
+++ backend/src/main/java/com/example/coosroombookings/model/BookingRequest.java
@@ -4,19 +4,20 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Setter
 @Getter
 public class BookingRequest {
-    // Getters and Setters
-    private String email;
-    private String roomName;
+    private Room room; // Include a Room
+    private List<User> users; // Include a List of Users
     private LocalDateTime startTime;
     private LocalDateTime endTime;
 
-    public BookingRequest(String email, String roomName, LocalDateTime startTime, LocalDateTime endTime) {
-        this.email = email;
-        this.roomName = roomName;
+    // Constructor
+    public BookingRequest(Room room, List<User> users, LocalDateTime startTime, LocalDateTime endTime) {
+        this.room = room;
+        this.users = users;
         this.startTime = startTime;
         this.endTime = endTime;
     }
